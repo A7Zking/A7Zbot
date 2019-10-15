fe5a0c74f9b24ad418f2ac8cd27ee07e3da72130
A7Zking <56368745+A7Zking@users.noreply.github.com>
2019-10-15 22:15:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -121,15 +121,15 @@ member.guild.fetchInvites().then(guildInvites => {
                               if (err) return console.log(err);
       
                                     ctx.font = "bold 12px Arial";
-                              ctx.fontSize = '35px';
+                              ctx.fontSize = '40px';
                               ctx.fillStyle = "#edd28f";
-                                ctx.fillText(member.user.username, 200, 150);
+                                ctx.fillText(member.user.username, 220, 170);
                               
                               //NAMEً
                               ctx.font = "bold 12px Arial";
                               ctx.fontSize = '40px';
                               ctx.fillStyle = "#edd28f";
-      ctx.fillText(`Marshal Discord Server`, 260, 125);
+      ctx.fillText(`Marshal Discord`, 260, 125);
       
                               //AVATARً
                               let Avatar = Canvas.Image;
