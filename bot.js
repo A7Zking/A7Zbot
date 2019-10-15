cc421fbe2c00048eaa31335ca2790cba35c26289
A7Zking <56368745+A7Zking@users.noreply.github.com>
2019-10-15 22:27:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,8 +93,8 @@ member.guild.fetchInvites().then(guildInvites => {
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
     const yumz = member.guild.channels.find("name", `${sChannel}`);
-     yumz.send(`<@${member.user.id}> joined by <@${inviter.id}>`);
-     yumz.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+    const yumz.send(`<@${member.user.id}> joined by <@${inviter.id}>`);
+    const yumz.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
   }); 
       var Canvas = require('canvas')
       var jimp = require('jimp')
